4cd2f4cf10a53f4e5a2f2a1046b21af8b3e22c56
Tarick <gabre.tarick@gmail.com>
2022-10-30 15:35:38 -0300

    classe PVE

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -13,9 +13,10 @@ export default class PVP extends Battle {
   fight(): number {
     while (this._character1.lifePoints > 0 
        && this._character2.lifePoints > 0) {
-      this._character1.attack(this._character2);
+      this._character1.attack(this._character2);      
       this._character2.attack(this._character1);
     }
+
     return this._character1.lifePoints === -1 ? -1 : 1;
   }
 }
